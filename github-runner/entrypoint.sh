5935bac9516d5de32f902b5efa63737221b2829b
anirudhshingala <anirudh.shingala@deepintent.com>
2024-05-19 13:52:23 +0530

    fix: token variable

--- github-runner/entrypoint.sh
+++ github-runner/entrypoint.sh
@@ -4,7 +4,7 @@ RUNNER_TOKEN=$(curl -sX POST -H "Authorization: token ${GITHUB_PAT}" \
   https://api.github.com/repos/${GITHUB_OWNER_REPO}/actions/runners/registration-token \
   | jq .token --raw-output)
 
-./config.sh --url https://github.com/${GITHUB_OWNER_REPO} --token ${TOKEN}
+./config.sh --url https://github.com/${GITHUB_OWNER_REPO} --token ${RUNNER_TOKEN}
 
 cleanup() {
   ./config.sh remove --token ${TOKEN}
