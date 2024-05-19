bc3c066c9d8152ed1936bb87c80a16922c372ecc
anirudhshingala <anirudh.shingala@deepintent.com>
2024-05-19 14:27:34 +0530

    adding name lable as hostname to avoid error of runner registering itself and failing

--- github-runner/entrypoint.sh
+++ github-runner/entrypoint.sh
@@ -4,12 +4,18 @@ RUNNER_TOKEN=$(curl -sX POST -H "Authorization: token ${GITHUB_PAT}" \
   https://api.github.com/repos/${GITHUB_OWNER_REPO}/actions/runners/registration-token \
   | jq .token --raw-output)
 
-./config.sh --url https://github.com/${GITHUB_OWNER_REPO} --token ${RUNNER_TOKEN}
+./config.sh --url https://github.com/${GITHUB_OWNER_REPO} --token ${RUNNER_TOKEN} --name $(hostname) --labels self-hosted,cicd-python
 
 cleanup() {
-  ./config.sh remove --token ${TOKEN}
+  ./config.sh remove --unattended --token ${TOKEN}
 }
 
+deregister_runner() {
+  echo "Deregistering Runner"
+  ./config.sh remove --unattended --token ${TOKEN}
+}
+
+trap 'deregister_runner; exit 130' SIGTERM
 trap 'cleanup; exit 130' INT
 trap 'cleanup; exit 143' TERM
 
